7d07040e1869759c197c355348a6f880f36d5021
李祎 <1642670230@qq.com>
2023-10-01 12:03:23 +0800

    10.1上午提交

---
+++ mybatis/src/main/java/cn/edu/ctbu/mybatis/dao/SysDictDAO.java
@@ -0,0 +1,24 @@
+package cn.edu.ctbu.mybatis.dao;
+
+import cn.edu.ctbu.mybatis.domain.SysDictDO;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface SysDictDAO {
+
+    @Results({
+            @Result(property = "id",column = "id"),
+            @Result(property = "name",column = "name"),
+            @Result(property = "value",column = "value"),
+            @Result(property = "type",column = "type"),
+            @Result(property = "description",column = "description")
+    })
+
+    @Select("SELECT * FROM sys_dict WHERE id=#{id}")
+    public List<SysDictDO> get(int id);
+
+    @Insert("INSERT into sys_dict(name,value)values(#(name),#(value))")
+    public void save(SysDictDO sysDictDO);
+}
--- mybatis/src/main/java/cn/edu/ctbu/mybatis/domain/SysDictDO.java
+++ mybatis/src/main/java/cn/edu/ctbu/mybatis/domain/SysDictDO.java
@@ -1,19 +1,27 @@
 package cn.edu.ctbu.mybatis.domain;
 
+import lombok.Data;
+
+@Data
 public class SysDictDO {
     /**
-     * id,主键，自增
+     * id，主键，自增
      */
     private Long id;
     /**
-     * name,名称
+     * name，名称
      * */
     private String name;
     /**
-     *
+     * value，值
      * */
     private String value;
+    /**
+     * type，类型
+     * */
     private String type;
+    /**
+     * description，描述
+     * */
     private String description;
-
 }
