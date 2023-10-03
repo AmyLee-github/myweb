2f7e2208b2c9bd5a1cfe69ba10a25247b5ff8073
李祎 <1642670230@qq.com>
2023-10-03 11:31:49 +0800

    mybatisplus项目做完

--- mybatisplus/src/test/java/cn/edu/ctbu/mybatisplus/mapper/UserMapperTest.java
+++ mybatisplus/src/test/java/cn/edu/ctbu/mybatisplus/mapper/UserMapperTest.java
@@ -4,13 +4,16 @@ import cn.edu.ctbu.mybatisplus.domain.User;
 import com.baomidou.mybatisplus.core.toolkit.Assert;
 import junit.framework.TestCase;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-@SpringBootTest
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserMapperTest extends TestCase {
     @Autowired
     private UserMapper userMapper;
